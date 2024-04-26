2970367c37629e0917e289ed81ea00603fbbc9f8
abdelali03 <70915761+abdelali03@users.noreply.github.com>
2024-04-27 00:53:40 +0100

    Delete

--- library-management-system-main/src/main/java/net/java/lms/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package net.javaguides.lms.repository;
-
-import net.javaguides.lms.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
