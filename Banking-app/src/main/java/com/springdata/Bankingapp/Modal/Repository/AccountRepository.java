0e9f0f8db31a7c91b3ebc85823861f9ce633d71c
abdelali03 <70915761+abdelali03@users.noreply.github.com>
2024-04-27 00:53:57 +0100

    Delete

--- Banking-app/src/main/java/com/springdata/Bankingapp/Modal/Repository/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springdata.Bankingapp.Modal.Repository;
-
-import com.springdata.Bankingapp.Modal.Entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
